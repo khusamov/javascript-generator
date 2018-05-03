d6650c9bf3b9875811f6c68abe50f939a9ac1d56
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-03 16:15:10 +0300

    Добавлен вывод комментариев элементов массивов и объектов

--- src/lib/view/VectorNodeView.ts
+++ src/lib/view/VectorNodeView.ts
@@ -1,16 +1,23 @@
 import VectorNode from '../node/VectorNode';
 import NodeView from './NodeView';
 import Node from '../node/Node';
+import Comment from '../code/Comment';
 
 export default abstract class VectorNodeView extends NodeView {
 	protected node: VectorNode;
 	protected brackets;
 	protected itemDivider;
 	protected bracketDivider;
+	protected EOL: string = '\n';
 	toString(): string {
 		return [
 			this.brackets[0],
-			this.node.items.map(this.itemToString.bind(this)).join(this.itemDivider),
+			this.node.items.map(item => {
+				const result: string[] = [];
+				if (item.comment) result.push(new Comment(item.comment).toCodeString());
+				result.push(this.itemToString(item));
+				return result.join(this.EOL);
+			}).join(this.itemDivider),
 			this.brackets[1]
 		].join(this.bracketDivider);
 	}
