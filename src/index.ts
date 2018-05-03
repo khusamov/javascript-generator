7bb19bf768f15996fc3f156146cb9fa7dfb86b69
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-03 18:41:55 +0300

    Расширен АПИ модуля

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
 export { default as Node } from './lib/node/Node';
 
-export { default as StringNode } from './lib/node/scalar/StringNode';
 export { default as BooleanNode } from './lib/node/scalar/BooleanNode';
-export { default as NumberNode } from './lib/node/scalar/NumberNode';
+export { default as DateNode } from './lib/node/scalar/DateNode';
+export { default as ExpressionNode } from './lib/node/scalar/ExpressionNode';
 export { default as FunctionNode } from './lib/node/scalar/FunctionNode';
+export { default as NullNode } from './lib/node/scalar/NullNode';
+export { default as NumberNode } from './lib/node/scalar/NumberNode';
+export { default as RegExpNode } from './lib/node/scalar/RegExpNode';
+export { default as StringNode } from './lib/node/scalar/StringNode';
+export { default as UndefinedNode } from './lib/node/scalar/UndefinedNode';
 export { default as ArrayNode } from './lib/node/vector/ArrayNode';
 export { default as ObjectNode } from './lib/node/vector/ObjectNode';
 
-export { default as StringNodeView } from './lib/view/scalar/StringNodeView';
 export { default as BooleanNodeView } from './lib/view/scalar/BooleanNodeView';
-export { default as NumberNodeView } from './lib/view/scalar/NumberNodeView';
+export { default as DateNodeView } from './lib/view/scalar/DateNodeView';
+export { default as ExpressionNodeView } from './lib/view/scalar/ExpressionNodeView';
 export { default as FunctionNodeView } from './lib/view/scalar/FunctionNodeView';
+export { default as NullNodeView } from './lib/view/scalar/NullNodeView';
+export { default as NumberNodeView } from './lib/view/scalar/NumberNodeView';
+export { default as RegExpNodeView } from './lib/view/scalar/RegExpNodeView';
+export { default as StringNodeView } from './lib/view/scalar/StringNodeView';
+export { default as UndefinedNodeView } from './lib/view/scalar/UndefinedNodeView';
 export { default as ArrayNodeView } from './lib/view/vector/ArrayNodeView';
 export { default as ObjectNodeView } from './lib/view/vector/ObjectNodeView';
 
+export { default as BooleanCode } from './lib/code/scalar/BooleanCode';
+export { default as DateCode } from './lib/code/scalar/DateCode';
+export { default as ExpressionCode } from './lib/code/scalar/ExpressionCode';
+export { default as FunctionCode } from './lib/code/scalar/FunctionCode';
+export { default as NullCode } from './lib/code/scalar/NullCode';
+export { default as NumberCode } from './lib/code/scalar/NumberCode';
+export { default as RegExpCode } from './lib/code/scalar/RegExpCode';
+export { default as StringCode } from './lib/code/scalar/StringCode';
+export { default as UndefinedCode } from './lib/code/scalar/UndefinedCode';
 export { default as ArrayCode } from './lib/code/vector/ArrayCode';
 export { default as ObjectCode } from './lib/code/vector/ObjectCode';
