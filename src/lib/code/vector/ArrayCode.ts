2549ec221ac3d5605830727a66516e9b2a45ab47
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-21 14:08:49 +0300

    Добавлены недостающие публичные конструкторы

--- src/lib/code/vector/ArrayCode.ts
+++ src/lib/code/vector/ArrayCode.ts
@@ -2,4 +2,8 @@ import Node from '../../node/Node';
 import ArrayNode from '../../node/vector/ArrayNode';
 import Code from '../Code';
 
-export default class ArrayCode<T extends Node = ArrayNode> extends Code<T> {}
+export default class ArrayCode<T extends Node = ArrayNode> extends Code<T> {
+	constructor(node: T) {
+		super(node);
+	}
+}
--- src/lib/code/vector/ObjectCode.ts
+++ src/lib/code/vector/ObjectCode.ts
@@ -2,4 +2,8 @@ import Node from '../../node/Node';
 import ObjectNode from '../../node/vector/ObjectNode';
 import Code from '../Code';
 
-export default class ObjectCode<T extends Node = ObjectNode> extends Code<T> {}
+export default class ObjectCode<T extends Node = ObjectNode> extends Code<T> {
+	constructor(node: T) {
+		super(node);
+	}
+}
