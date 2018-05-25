aa42bd4b7391ac0cce04d9149abcad04c2b30a9d
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-25 09:44:31 +0300

    Добавлена возможность создания безымянного ExpressionNode узла при помощи функции шаблонизации

--- src/lib/node/scalar/ExpressionNode.ts
+++ src/lib/node/scalar/ExpressionNode.ts
@@ -1,3 +1,16 @@
 import ScalarNode from '../ScalarNode';
+import FunctionNode from './FunctionNode';
 
-export default class ExpressionNode extends ScalarNode<string> {}
+export default class ExpressionNode extends ScalarNode<string> {
+	/**
+	 * Создание безымянного ExpressionNode узла.
+	 * Функция шаблонизации (https://learn.javascript.ru/es-string).
+	 * @param {TemplateStringsArray} strings
+	 * @param {string} vars
+	 * @returns {FunctionNode}
+	 */
+	static nameless(strings: TemplateStringsArray, ...vars: string[]): ExpressionNode {
+		const str = strings.map((s, i) => s + (i === vars.length ? '' : vars[i])).join('');
+		return new this(undefined, str);
+	}
+}
