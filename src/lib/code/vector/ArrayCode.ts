8a4ac3774ca890f58bd3332b8a9347cbda079f94
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-01 16:30:37 +0300

    В классах ArrayCode и ObjectCode возвращена возможность указывать типы узла и вида узла

--- src/lib/code/vector/ArrayCode.ts
+++ src/lib/code/vector/ArrayCode.ts
@@ -2,8 +2,9 @@ import ArrayNode from '../../node/vector/ArrayNode';
 import VectorCode from '../VectorCode';
 import ArrayNodeView from '../../view/vector/ArrayNodeView';
 
-export default class ArrayCode extends VectorCode<ArrayNode, ArrayNodeView> {
-	constructor(protected node: ArrayNode) {
+export default class ArrayCode<T extends ArrayNode = ArrayNode,
+	U extends ArrayNodeView = ArrayNodeView> extends VectorCode<T, U> {
+	constructor(protected node: T) {
 		super(node);
 	}
 }
--- src/lib/code/vector/ObjectCode.ts
+++ src/lib/code/vector/ObjectCode.ts
@@ -2,8 +2,9 @@ import ObjectNode from '../../node/vector/ObjectNode';
 import VectorCode from '../VectorCode';
 import ObjectNodeView from '../../view/vector/ObjectNodeView';
 
-export default class ObjectCode extends VectorCode<ObjectNode, ObjectNodeView> {
-	constructor(protected node: ObjectNode) {
+export default class ObjectCode<T extends ObjectNode = ObjectNode,
+	U extends ObjectNodeView = ObjectNodeView> extends VectorCode<T, U> {
+	constructor(protected node: T) {
 		super(node);
 	}
 }
